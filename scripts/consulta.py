b10e8097695233a58d0cc80f3f24847edb87b124
maiquelwx <maiquel.141@gmail.com>
2026-06-18 16:19:20 -0300

    Remove pasta scripts do repositório

--- scripts/consulta.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://apisidra.ibge.gov.br/desctabapi.aspx?o=3&formato=json"
-
-r = requests.get(url)
-
-print(r.status_code)
-print(r.headers["content-type"])
-print(r.text[:500])
